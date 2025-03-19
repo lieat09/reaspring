ba5888aa65c7c084b9ca68bd20460b4517e39b18
pinjie.chen <pinjie.chen@pouchen.com>
2025-03-19 16:44:36 +0800

    Initial commit for ReaSpring project

---
+++ src/main/java/io/github/pinjie/reaspring/ReaspringApplication.java
@@ -0,0 +1,13 @@
+package io.github.pinjie.reaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pinjie/reaspring/ReaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pinjie.reaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
